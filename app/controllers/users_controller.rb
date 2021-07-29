74c89a8c193aecad119eb24f908a7ba5894fd52b
springkyu <seika.k.r0610@gmail.com>
2021-07-29 13:03:50 +0000

    [debug]応用課題２

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
-  
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -35,4 +35,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+  
 end
